dde7e80476e74ac55a0754c4ddbce433b74dabf5
Nick Davies <nickdavies100@gmail.com>
2020-08-18 20:06:46 -0700

    Update

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const createError = require('http-errors')
 const { projects } = require("./data.json");
 
-
 app.set('view engine', 'pug');
 
 app.use("/static", express.static("public"));
 app.get("/", (req, res) => {
   res.render("index", { projects });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.get("/projects/:id", (req, res) => {
